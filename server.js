f754d8232f8c468f85eb5e100cb464ef940ebeb6
JJimenez <j.jimenez.994@hotmail.com>
2019-01-28 21:30:10 -0500

    Starting a new project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000; 
+const app = express();
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors())
+
+require("./server/config/mongoose");
+app.use(express.static('client/build'));
+
+// passport middleware
+
+
+
+require("./server/config/routes")(app);
+
+app.listen(port, () => {
+  console.log(`Hey! you are in port ${port}`);
+});
+
