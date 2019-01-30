b0a6c635b828feb4fe35ae0d038675b036566fa3
JJimenez <j.jimenez.994@hotmail.com>
2019-01-29 23:18:14 -0500

    _

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,5 +2,6 @@ const auth = require('../controllers/auth')
 
 module.exports = (app) => {
   app.post('/api/register', auth.register);
-  app.get('/api/allUsers', auth.allUsers)
+  app.get('/api/allUsers', auth.allUsers);
+  app.post('/api/login', auth.login)
 }
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const User = mongoose.model('User');
+const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys')
 
 
 module.exports = {
@@ -9,13 +13,46 @@ module.exports = {
         if(result){
           res.status(400).json({error: "Email already exists"})
         }else{
+          req.body.password = bcrypt.hashSync(req.body.password, 10);
+          req.body.avatar = gravatar.url(req.body.email, {s: 200, r: "pg", d: "mm" })
           User.create(req.body)
-            .then(result => res.json(result))
+            .then(user => {
+              // this is what imformation you want to include in token generaten
+              const payload = {_id: user._id, email: user.email, firstName: user.firstName, lastName: user.lastName, avatar: user.avatar}
+              // generating a token with the payload, type "Bearer", and expiration time "1h"
+              jwt.sign(payload, keys.secretKey, {expiresIn: "1h"}, (err, token) => {
+                res.json({success: true, token : "Bearer " + token})
+              })
+            })
             .catch(err => res.status(405).json(err))
         }
       })
       .catch(err => res.status(405).json(err))
   },
+  login: (req, res) => {
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(user){
+          bcrypt.compare(req.body.password, user.password)
+            .then(isMatch => {
+              if(isMatch){
+                // this is what imformation you want to include in token generaten
+                const payload = {_id: user._id, email: user.email, firstName: user.firstName, lastName: user.lastName, avatar: user.avatar}
+                // generating a token with the payload, type "Bearer", and expiration time "1h"
+                jwt.sign(payload, keys.secretKey, {expiresIn: "1h"}, (err, token) => {
+                res.json({success: true, token : "Bearer " + token})})
+              }else{
+                res.status(400).json({error: "Password does not match"})
+              }
+            })
+        }else{
+          res.status(400).json({error: "Email not found 1"})
+        }
+      })
+      .catch(err => console.log(err))
+  },
+
+
   allUsers: (req, res) => {
     User.find()
       .then(result => res.json(result))
