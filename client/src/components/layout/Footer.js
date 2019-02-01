31d15f61ed9ca94722a4aafd68f27251797d7b06
JJimenez <j.jimenez.994@hotmail.com>
2019-01-31 23:14:34 -0500

    _

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
-
-class Footer extends Component {
-  render () {
-    return (
-      <div>
-        Footer
-      </div>
-    )
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Grid, Paper } from '@material-ui/core';
+const styles = {
+  paper: {
+    textAlign:'center'
   }
 }
 
-export default Footer
+class Footer extends Component {
+  render () {
+    const {classes} = this.props
+    return (
+      <Grid>
+        <Paper className={classes.paper}>
+          Copyright &copy; {new Date().getFullYear()} DevConnector
+        </Paper>
+
+      </Grid>
+          
+          
+        
+    )
+  }
+}
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+export default withStyles(styles)(Footer)
