c6b5ca1c144dc6462fe664bb40ed20665eb4a623
JJimenez <j.jimenez.994@hotmail.com>
2019-02-11 19:02:46 -0500

    _

---
+++ client/src/views/Profile/profileForm.js
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react'
+
+class ProfileForm extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  componentWillMount () {
+
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentWillReceiveProps (nextProps) {
+
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+
+  }
+
+  componentWillUnmount () {
+
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+ProfileForm.propTypes = {
+
+}
+
+export default connect(mapStateProps, {})(ProfileForm)
