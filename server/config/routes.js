ffff5b78a508b4ab4039cd186e1720f157a37461
JJimenez <j.jimenez.994@hotmail.com>
2019-03-05 19:13:13 -0500

    _

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,30 +1,34 @@
-const auth = require('../controllers/auth');
-const profile = require('../controllers/profile')
-const passport = require('passport');
-const education = require('../controllers/education');
-const experience = require('../controllers/experience');
+const auth = require("../controllers/auth");
+const profile = require("../controllers/profile");
+const passport = require("passport");
+const education = require("../controllers/education");
+const experience = require("../controllers/experience");
+const post = require("../controllers/post");
 
-
-module.exports = (app) => {
+module.exports = app => {
   // with auth routes can be assigned to become private
-  const private = passport.authenticate("jwt", {session: false});
+  const private = passport.authenticate("jwt", { session: false });
 
   // Auth
-  app.post('/api/register', auth.register);
-  app.post('/api/login', auth.login)
-  app.get('/api/allUsers', private, auth.allUsers);
+  app.post("/api/register", auth.register);
+  app.post("/api/login", auth.login);
+  app.get("/api/allUsers", private, auth.allUsers);
 
   // Profile
-  app.get('/api/profile', private, profile.findProfile);
-  app.post('/api/createOrUpdateProfile', private, profile.createOrUpdate);
-  app.get('/api/profiles', profile.allProfiles);
-  app.get('/api/profile/:username', profile.findProfileByUsername);
+  app.get("/api/profile", private, profile.findProfile);
+  app.post("/api/createOrUpdateProfile", private, profile.createOrUpdate);
+  app.get("/api/profiles", profile.allProfiles);
+  app.get("/api/profile/:username", profile.findProfileByUsername);
 
   // Education
-  app.post('/api/addEducation', private, education.addEducation);
-  app.delete('/api/deleteEducation/:id', private, education.deleteOne);
+  app.post("/api/addEducation", private, education.addEducation);
+  app.delete("/api/deleteEducation/:id", private, education.deleteOne);
 
-    // Experience
-    app.post('/api/addExperience', private, experience.addExperience);
-    app.delete('/api/deleteExperience/:id', private, experience.deleteOne);
-} 
+  // Experience
+  app.post("/api/addExperience", private, experience.addExperience);
+  app.delete("/api/deleteExperience/:id", private, experience.deleteOne);
+
+  // Post
+  app.get("/api/getAll", private, post.allPosts);
+  app.post("/api/createPost", private, post.createPost);
+};
