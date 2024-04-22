2da29f866a890a6a08e2e0c4575393d8eaec84b6
JJimenez <j.jimenez.994@hotmail.com>
2024-04-21 21:42:09 -0400

    fix github sections

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -263,16 +263,16 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', async (req, res) => {
   try {
     
-    // const uri = encodeURI(
-    //   `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
-    // );
-    // const headers = {
-    //   'user-agent': 'node.js',
-    //   Authorization: `token ${config.get('githubToken')}`
-    // };
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${process.env.GITHUB_TOKEN}`
+    };
 
-    // const gitHubResponse = await axios.get(uri, { headers });
-    // return res.json(gitHubResponse.data);
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
   } catch (err) {
     console.error(err.message);
     return res.status(404).json({ msg: 'No Github profile found' });
